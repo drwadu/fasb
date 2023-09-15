801442a4adab4b90a98419490812da4fd7549946
drwadu <dominik.rusovac@hotmail.com>
2023-09-15 08:58:20 +0200

    basic implementation.

--- experiments/run_600s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for instance in ./$1/*.lp; do
-	s=$instance
-	a=(${s//// })
-	file=${a[3]}
-	echo $instance
-	perf stat --timeout 600000 bins/fasb $instance 0 $2 &> $3/$file.out
-    done
--- experiments/run_h1.sh
+++
@@ -1,2 +0,0 @@
-sh run_600s.sh instances/jsd_pc-config fsb-scripts/h1.fsb outs/h1-sgo-fc__jsd_pc-config/
-
